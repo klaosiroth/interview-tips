1962f4a8eed1917e24f3b3e99baa24f5a1eb52d6
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 17:13:10 +0700

    Course Introduction

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Front End Interview Questions: Introduction
+ *
+ * 1. Lets review the question.
+ * 2. Pause the screencast and spend 5 - 10 minutes researching an answer.
+ * 3. Come back to my answer / solution.
+ * 4. Review, record and repeat till you can confidently answer the question.
+ */

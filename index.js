68c079ac1f745dc9368bf56b1723cc18661d489c
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 17:14:49 +0700

    const vs let vs var

--- index.js
+++ index.js
@@ -44,3 +44,27 @@ if (word.length > 0) {
 if (false) {
   console.log('false');
 }
+
+/**
+ * Front End Interview Questions: const vs let vs var
+ *
+ * Q: What are the differences between const, let, and var?
+ */
+
+// var firstNameVar;
+
+if (false) {
+  let firstNameLet = 'Dylan';
+  var firstNameVar = 'Dylan';
+}
+
+// console.log(firstNameVar);
+// console.log(firstNameLet);
+
+const PI = 3.14;
+const user = {
+  firstName: 'D-Money',
+};
+// PI = 3.145;
+console.log(PI);
+user.firstName = 'Dylan';

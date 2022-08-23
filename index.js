e882bc2ab87d0fa06aaee15dd43bca48ff6a028e
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 17:13:29 +0700

    Falsy Values

--- index.js
+++ index.js
@@ -6,3 +6,41 @@
  * 3. Come back to my answer / solution.
  * 4. Review, record and repeat till you can confidently answer the question.
  */
+
+/**
+ * Front End Interview Questions: Falsy Values
+ *
+ * Q: What are falsey values in JavaScript?
+ */
+
+if (null) {
+  console.log('null');
+}
+
+if (undefined) {
+  console.log('undefined');
+}
+
+if (0) {
+  console.log('0');
+}
+
+const arr = [];
+
+if (arr.length > 0) {
+  console.log('0');
+}
+
+if ('') {
+  console.log('empty');
+}
+
+const word = '';
+
+if (word.length > 0) {
+  console.log('empty');
+}
+
+if (false) {
+  console.log('false');
+}

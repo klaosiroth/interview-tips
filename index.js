096f8f759dc9a16eef4b70bb1275da9e4eb31ae1
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 17:27:48 +0700

    Pass by Value vs Pass by Reference

--- index.js
+++ index.js
@@ -251,3 +251,29 @@ async function getPost() {
 }
 
 getPost();
+
+/**
+ * Front End Interview Questions: Pass by Value vs Pass by Reference
+ *
+ * Q: What is pass by reference and how does it differ from pass by value?
+ */
+
+const prim = 5;
+
+function add(value) {
+  value++;
+
+  return value;
+}
+
+console.log(add(prim), prim);
+
+const ref = { count: 5 };
+
+function add2(value) {
+  value.count++;
+
+  return value.count;
+}
+
+console.log(add2(ref), ref.count);
